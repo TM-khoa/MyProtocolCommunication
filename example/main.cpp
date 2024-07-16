9f8226609aefa12475970c144947a57eb30cafbc
TM-khoa <minhkhoa9a31314@gmail.com>
2024-07-16 23:59:34 +0700

    Successfully send and receive test message

--- example/main.cpp
+++ example/main.cpp
@@ -1,8 +1,150 @@
+/*                                                                                                         
+ ██                           ░░                                              ░▓▒                          ▓█ 
+ ███▒                        ▓███                                           ░████░                       ░███ 
+ ▒████░                      ▓████▒                                        ▒█████░                      ▓███▓ 
+  █████▓                     ▒█████▓                                      ███████░                    ▒█████░ 
+  ███████▒                   ░██████▓                                    ███▓█▓██░                  ░███████  
+  ▓██▓█████                  ░███▓▓▒▓▒                                  █████▓███░                ░████████▓  
+  ░███▓█▓███▒                ▒██▓▒▒▒▒▓░                                ▒█▓███████                ▒████▓█▓██░  
+   ██▓█▓█▓████               ▒█▒▒▒▒▒▒▒▒░                              ░▓▒▒▒▓▓▓███               █████▓█▓███   
+   ▓██▓█▓█▓████░             ░▓▒▒▒▒▒▒▒▒▒░                             ▒▒▒▒▒▒▒▒▓█▓             ░█████▓█▓████   
+   ▒███▓█▓█▓████▓            ░▒▒▒▒▒▒▒░░░▒░                           ▒▒▒▒▒▒▒▒▒▒▓▒            ▒███▓█▓█▓█▓██▓   
+   ░██▓█▓█▓█▓████▓           ░▒▒░░░░░░░░░▒                          ░▒░░░░░▒▒▒▒▒            ▓█████▓█▓█▓███▒   
+   ░███▓█▓█▓█▓█████           ▒░░░░░░░░░░░░                        ░▒░░░░░░░░░▒▒           ▓███▓█▓█▓█▓█▓██░   
+    ████▓█▓█▓█▓█████          ▒░░░░░░░░░░░░░                      ░▒░░░░░░░░░▒▒░          ████▓█▓█▓█▓█▓███    
+    ███▓█▓█▓█▓█▓█▓███         ░░░░░░░░░░░░░░                      ▒░░░░░░░░░░░▒          ████▓█▓█▓█▓█▓████    
+    ▒███▓█▓█▓█▓█▓█▓███        ░▒░░░░░░░░░░░▒░                    ░▒░░░░░░░░░░░░         ████▓█▓█▓█▓█▓█▓██▓    
+    ░██▓█▓█▓█▓█▓█▓████▒        ▒▒░░░░░░░░░░▒▒                    ▒▒░░░▒░▒░░░░░░        ▒███▓█▓█▓█▓█▓█▓███░    
+     ███▓█▓█▓█▓█▓█▓███▓░       ░▒▒░▒▒▒▒▒▒▒░▒▒     ░░░░░░░░░     ░▒▒▒▒░░░░░░░░▒        ░▒▓█▓█▓█▓█▓█▓█▓████     
+     ▒███▓█▓█▓█▓█▓█▓█▓▒▒░       ░▒▒░░░░░▒▒▒▒▒▒▓▒▓▓▓░▒▒▒▓▓▓▓▓▓▓▓▒▒▒▒░░▒▒░▒▒▒▒▒░       ░▒░▓██▓█▓█▓█▓█▓█▓██▒     
+      ████▓█▓█▓█▓█▓██▓▒▒▒         ░░░░ ░▒▒░░ ░░░░▒░ ░░ ░░░░░▒█░  ░░ ░░░░░░▒▒░        ▒▒▒▓█▓█▓█▓█▓█▓█▓███      
+      ▒██▓█▓█▓█▓█▓█▓███▒▒░         ░▒▒▒▒▓▓▒  ░  ░░  ▒░▒▒▒░░░░  ░▒░░▒▒▒▒░░░░         ░▒▓████▓█▓█▓█▓█▓███▒      
+       ███▓█▓█▓█▓█▓█▓███▓▓         ▒▒▒▓▓▒▒▒░░      ░▒▒▒░▒▓░ ░ ▒▓▒▒▒▓▓▓▒▒▓▒▒░        ▓█████▓█▓█▓█▓█▓████       
+       ░███▓█▓█▓█▓█▓█▓█████      ▒▒░  ░    ░░▒▒▒░   ░▒▒░    ░░ ▒▒▒░ ░   ░ ░▒▒      ▓███▓█▓█▓█▓█▓█▓█▓██▒       
+        ▓███▓█▓█▓█▓█▓█▓█▓██▒   ░▓▓▓▒▒░ ░▒▒   ░▓▒▒░       ░░░░        ░▒▒░▒▒▓▓█▒   ▒███▓█▓█▓█▓█▓█▓█▓███        
+         ████▓█▓█▓█▓█▓█▓█▓██  ▓█████▓▒▒▒▒▓▒           ░░░     ▒▓▒▓█████████████▓  ███▓█▓█▓█▓█▓█▓█▓███         
+          ████▓█▓█▓█▓█▓█▓███▓▒█████▓░░ ░░  ▓██░ ░          ░▒▓██▓██▓░░░░████████▒▒██▓█▓█▓█▓█▓█▓█████░         
+           ████▓█▓█▓█▓█▓█▓█████▓███▓▒▒▓▓▒▒▓████▓██             ██▓░▒░░▒░▒▓███▓█████▓█▓█▓█▓█▓█▓█████░          
+            ████▓█▓█▓█▓█▓█▓█▓██████▓███████▓▓████░           ▒  ▒▒░░ ▒▓▓▒▒████▓█████▓█▓█▓█▓█▓█████            
+             ▓█████▓█▓█▓█▓█▓█▓███▓    ░▒███▓███░          ░▓███▒░░▒▓██▒░    ▓██▓█▓█▓█▓█▓█▓█▓████▓             
+              ░█████▓█▓█▓█▓█▓███▓        ░█████░          ███▓██████▒        ███▓█▓█▓█▓█▓█▓████▒              
+                ▓██████▓█▓█▓█▓██           ▓████      ░  ▒███▓█████           ███▓█▓█▓█▓█████▓                
+                 ░██████▓█▓█▓███     ▒▓▓░   █████     ░░  ▓███████   ░▒▒░     ██▓█▓█▓█▓█████░                 
+                   ▒█████▓█▓█▓██    ██████  ░████▒  ░ ░    ░█████   █████▒    ███▓█▓██████▒                   
+                     ▒████▓█▓███░   ██████   ░▒▒░    ░░      ▓▓██   █████▓   ░██▓█▓█████▒                     
+                       ▒█████████    ▒▓█▓       ░  ░░▒░         ▓   ░▓▓▓░    █████████▓                       
+                         ▒████████░            ░▒░░▒░ ▒░▒░  ░░ ░           ▒████████▒                         
+                           ░████████▓        ▒▒  ░▓░  ░ ▒█░ ░░░░░         ▓███████░                           
+                             ▒██████▒░░░░  ░ ░░░░░   ░ ░   ░░░░░░░░░░░░░░░   ▒▓█▓                             
+                              ░█▓     ░░   ░░                 ░░░░░░░░░░░░░                                   
+                                  ░░░░░░░░░░░░░░              ░░░░░░ ░ ░ ██░░░                                
+                                   ░▒░░░██▒ ▒█▓░░░▒         ░░░ ░█▒ ▓▒▓▒░▓█░                                  
+                                     ░ ░██▒░░██░ ░░░░░  ░░▒▒░░░░░░░░░░░░░░                                    
+                                           ░░▒░░░░▒▒░░░░░░░░░░░░░░░░                                          
+                                                ░░░░░░░░░░░▒░▒▒                                                                                                                                                                                                                                                                                                  
+*/    
+
 #include "../Protocol/Protocol.h"
 #include <iostream>
+#include "MessageHandle.h"
 using namespace std;
 
+
+uint8_t testBuffer[100];
+
+
+void ReceiverGetData(FrameData fd){
+    std::cout << "Event: Receiver get message" << std::endl;
+}
+
+void SenderGetData(FrameData fd){
+    std::cout << "Event: Sender get message" << std::endl;
+}
+
+void ReceiverGetError(ProtocolErrorCode err){
+    std::cout << "Event: Receiver error message event found:" << (int)err << std::endl;
+}
+
+
+void SenderGetError(ProtocolErrorCode err){
+    std::cout << "Event: Sender error message event found:" << (int)err << std::endl;
+}
+
+MessageHandle Sender(&SenderGetData, &SenderGetError), Receiver(&ReceiverGetData, &ReceiverGetError);
+
+/**
+ * @brief Giả lập ngoại vi của MCU gửi dữ liệu, ngoại vi mỗi lần chỉ copy một byte
+ * dữ liệu từ RAM vào bộ đệm ngoại vi để gửi đi
+ */
+void SimulatePeripheralSendMessage(){
+    static uint8_t i = 0;
+    uint8_t periphBuffer[1] = {0};
+    FrameData fd = Sender.GetFrameDataInfo();
+    // Nếu frame data = 0 thì return
+    if(fd.totalLength == 0) return;
+    // Thực hiện copy 1 byte dữ liệu từ bộ đệm _txBuffer trong class Protocol vào bộ đệm ngoại vi
+    memcpy(periphBuffer, Sender.GetAddressTxBuffer() + i, 1);
+    // Gửi dữ liệu bộ đệm ngoại vi vào bộ đệm test để Receiver nhận
+    memcpy(testBuffer + i, periphBuffer, 1);
+    cout << "Sending byte: " << (int)i << " value: " << (int)*(Sender.GetAddressTxBuffer() + i)<< endl;
+    i++;
+    // Nếu đủ độ dài thì đã kết thúc frame truyền, thực hiện xóa bộ đệm _txBuffer
+    if(i == fd.totalLength) {
+        i = 0;
+        Sender.ResetFrame();
+        cout << "Sending complete" << endl;
+    }
+}
+
+/**
+ * @brief Giả lập ngoại vi của MCU nhận dữ liệu, ngoại vi mỗi lần chỉ copy một byte
+ * dữ liệu từ bộ đệm ngoại vi vào RAM
+ */
+void SimulatePeripheralReceiveMessage(){
+    static uint8_t i = 0;
+    uint8_t periphBuffer[1] = {0};
+    FrameData fd = Receiver.GetFrameDataInfo();
+    if(testBuffer[0] == 0) return;
+    if(fd.totalLength == 0) {
+        fd.totalLength = testBuffer[0];
+        // Set tổng độ dài frame truyền vào biến private FrameData trong class Protocol
+        Receiver.SetTotalLength(fd.totalLength);
+        memcpy(periphBuffer,testBuffer,1);
+        memcpy(Receiver.GetAddressRxBuffer() + i, periphBuffer, 1);
+        cout << "Receive byte: " << (int)i << " total length: " << (int)fd.totalLength << endl;
+        i++;
+    } 
+    else {
+        memcpy(periphBuffer,testBuffer + i, 1);
+        memcpy(Receiver.GetAddressRxBuffer() + i, periphBuffer, 1);
+        cout << "Receive byte: " << (int)i << " value: " << (int)*(Receiver.GetAddressRxBuffer() + i) << endl;
+        i++;
+        // Nếu đủ độ dài thì đã kết thúc frame truyền, thực hiện xóa bộ đệm _txBuffer
+        if(i == fd.totalLength) {
+            cout << "Receive complete" << endl;
+            // Xóa bộ đệm test sau khi nhận đầy đủ
+            memset(testBuffer, 0, i);
+            // Sau khi nhận đầy đủ khung truyền thì giải mã khung truyền, kiểm tra CRC16
+            Receiver.HandleReceiveMessage();
+            Receiver.ResetFrame();
+            i = 0;
+        }
+    }
+    
+}
+
+
+
+void BasicTest(){
+    uint8_t testValueSend = 10;
+    Sender.SendMessage((void*)&testValueSend, sizeof(uint8_t), PROTOCOL_ID_1);
+}
+
 int main(void){
-    cout << "Makefile example";
+    BasicTest();
+    while(1) {
+        SimulatePeripheralSendMessage();
+        SimulatePeripheralReceiveMessage();
+    }
     return 0;
 }

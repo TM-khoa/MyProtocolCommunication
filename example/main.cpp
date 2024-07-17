168e423e9e38d878eafa5af63e1bbe4e3eb90f1c
TM-khoa <minhkhoa9a31314@gmail.com>
2024-07-17 19:06:26 +0700

    feat: change sender, receiver to device 1 and 2, test handshake code ok

--- example/main.cpp
+++ example/main.cpp
@@ -47,30 +47,93 @@
 #include "../Protocol/Protocol.h"
 #include <iostream>
 #include "MessageHandle.h"
+#include "algorithm"
 using namespace std;
 
 
 uint8_t testBuffer[100];
+void Device2GetData(FrameData fd);
+void Device1GetData(FrameData fd);
+void Device2GetError(ProtocolErrorCode err);
+void Device1GetError(ProtocolErrorCode err);
+MessageHandle Device1(&Device1GetData, &Device1GetError), Device2(&Device2GetData, &Device2GetError);
 
-
-void ReceiverGetData(FrameData fd){
-    std::cout << "Event: Receiver get message" << std::endl;
+void Device2GetData(FrameData fd){
+    if(fd.protocolID == PROTOCOL_ID_HANDSHAKE){
+        uint8_t *code = new (nothrow) uint8_t[fd.payloadLength]; 
+        uint8_t *comparator = Device2.GetHandshakeCode();
+        // bắt buộc phải memset 0, nếu không sẽ bị segmentation fault
+        memset(code,0,fd.payloadLength);
+        Device2.GetValueFromPayload((void*)code,fd.payloadLength);
+        // Nếu bắt tay được thì 2 bên đang ở chế độ giải mã gói tin theo class Protocol
+        if(memcmp(code,comparator,fd.payloadLength) == 0 && Device2.GetHandshakeStatus() == false) {
+            std::cout << "Device2 matched handshake code, in protocol mode" << std::endl;
+            Device2.SetHandshake(true);
+            if(fd.requestData) {
+                Device2.ResponseHandshake();
+                std::cout << "Device2 response handshake code" << std::endl;
+            }
+            
+        }
+        delete[] code;
+        return;
+    } 
+    // Nếu không bắt tay được thì bỏ qua gói tin, không ở chế độ giải mã gói tin theo class Protocol
+    else if (Device2.GetHandshakeStatus() == false) { 
+        /* Nhận dữ liệu bình thường không thông qua xử lý khung truyền*/
+        std::cout << "Device2 not in protocol mode, receive data" << std::endl;
+        Device2.ResetFrame();
+        return;
+    }
+    std::cout << "Event: Device2 get message" << std::endl;
+    if(fd.requestData != true){
+        if(fd.protocolID == PROTOCOL_ID_1){
+            uint8_t testValueReceive = 0;
+            Device2.GetValueFromPayload((void*)&testValueReceive,sizeof(uint8_t));
+            std::cout << "Extract value receive from frame: "<< std::dec << (int)testValueReceive << std::endl;
+        }
+    }
 }
 
-void SenderGetData(FrameData fd){
-    std::cout << "Event: Sender get message" << std::endl;
+
+void Device2GetError(ProtocolErrorCode err){
+    std::cout << "Event: Device2 error message event found:" << (int)err << std::endl;
 }
 
-void ReceiverGetError(ProtocolErrorCode err){
-    std::cout << "Event: Receiver error message event found:" << (int)err << std::endl;
+void Device1GetData(FrameData fd){
+    if(fd.protocolID == PROTOCOL_ID_HANDSHAKE){
+        uint8_t *code1 = new (nothrow) uint8_t[fd.payloadLength]; 
+        uint8_t *comparator = Device1.GetHandshakeCode();
+        // bắt buộc phải memset 0, nếu không sẽ bị segmentation fault
+        memset(code1,0,fd.payloadLength);
+        Device1.GetValueFromPayload((void*)code1,fd.payloadLength);
+        // Nếu bắt tay được thì 2 bên đang ở chế độ giải mã gói tin theo class Protocol
+        if(memcmp(code1,comparator,fd.payloadLength) == 0 && Device1.GetHandshakeStatus() == false) {
+            std::cout << "Device1 matched handshake code, in protocol mode" << std::endl;
+            Device1.SetHandshake(true);
+            if(fd.requestData) {
+                Device1.ResponseHandshake();
+                std::cout << "Device1 response handshake code" << std::endl;
+            }    
+        }
+        delete[] code1;
+        return;
+    } 
+    // Nếu không bắt tay được thì bỏ qua gói tin, không ở chế độ giải mã gói tin theo class Protocol
+    else if (Device1.GetHandshakeStatus() == false) { 
+        /* Nhận dữ liệu bình thường không thông qua xử lý khung truyền*/
+        std::cout << "Device1 not in protocol mode, receive data" << std::endl;
+        Device1.ResetFrame();
+        return;
+    }
+    std::cout << "Event: Device1 get message" << std::endl;
+}
+
+void Device1GetError(ProtocolErrorCode err){
+    std::cout << "Event: Device1 error message event found:" << (int)err << std::endl;
 }
 
 
-void SenderGetError(ProtocolErrorCode err){
-    std::cout << "Event: Sender error message event found:" << (int)err << std::endl;
-}
-
-MessageHandle Sender(&SenderGetData, &SenderGetError), Receiver(&ReceiverGetData, &ReceiverGetError);
 
 /**
  * @brief Giả lập ngoại vi của MCU gửi dữ liệu, ngoại vi mỗi lần chỉ copy một byte
@@ -79,19 +142,31 @@ MessageHandle Sender(&SenderGetData, &SenderGetError), Receiver(&ReceiverGetData
 void SimulatePeripheralSendMessage(){
     static uint8_t i = 0;
     uint8_t periphBuffer[1] = {0};
-    FrameData fd = Sender.GetFrameDataInfo();
+    FrameData fd;
+    MessageHandle *targetMesg = NULL;
+    if(Device1.GetSendingStatus()){
+        targetMesg = &Device1;
+        cout << "Device1--->" << "\t";
+    }
+    else if(Device2.GetSendingStatus()){
+        targetMesg = &Device2;
+        cout << "Device2--->" << "\t";
+    }
+    else return;
+    fd = targetMesg->GetFrameDataInfo();
+
     // Nếu frame data = 0 thì return
     if(fd.totalLength == 0) return;
     // Thực hiện copy 1 byte dữ liệu từ bộ đệm _txBuffer trong class Protocol vào bộ đệm ngoại vi
-    memcpy(periphBuffer, Sender.GetAddressTxBuffer() + i, 1);
-    // Gửi dữ liệu bộ đệm ngoại vi vào bộ đệm test để Receiver nhận
+    memcpy(periphBuffer, targetMesg->GetAddressTxBuffer() + i, 1);
+    // Gửi dữ liệu bộ đệm ngoại vi vào bộ đệm test để Device2 nhận
     memcpy(testBuffer + i, periphBuffer, 1);
-    cout << "Sending byte: " << (int)i << " value: " << (int)*(Sender.GetAddressTxBuffer() + i)<< endl;
+    cout << "Sending byte: " << (int)i << " value: 0x" << std::hex << (int)*(targetMesg->GetAddressTxBuffer() + i)<< endl;
     i++;
     // Nếu đủ độ dài thì đã kết thúc frame truyền, thực hiện xóa bộ đệm _txBuffer
     if(i == fd.totalLength) {
         i = 0;
-        Sender.ResetFrame();
+        targetMesg->ResetFrame();
         cout << "Sending complete" << endl;
     }
 }
@@ -103,45 +178,56 @@ void SimulatePeripheralSendMessage(){
 void SimulatePeripheralReceiveMessage(){
     static uint8_t i = 0;
     uint8_t periphBuffer[1] = {0};
-    FrameData fd = Receiver.GetFrameDataInfo();
+    FrameData fd;
+    MessageHandle *targetMesg = NULL;
+    // Nếu bên gửi là device 1 thì bên nhận phải lấy thông tin của device 2
+    if(Device1.GetSendingStatus()){
+        targetMesg = &Device2;
+        cout << "Device2--->"  << "\t";
+    }
+    else if(Device2.GetSendingStatus()){
+        targetMesg = &Device1;
+        cout << "Device1--->"  << "\t";
+    }
+    else return;
+    fd = targetMesg->GetFrameDataInfo();
     if(testBuffer[0] == 0) return;
     if(fd.totalLength == 0) {
+        memcpy(periphBuffer,testBuffer,1);
         fd.totalLength = testBuffer[0];
         // Set tổng độ dài frame truyền vào biến private FrameData trong class Protocol
-        Receiver.SetTotalLength(fd.totalLength);
-        memcpy(periphBuffer,testBuffer,1);
-        memcpy(Receiver.GetAddressRxBuffer() + i, periphBuffer, 1);
-        cout << "Receive byte: " << (int)i << " total length: " << (int)fd.totalLength << endl;
+        targetMesg->SetTotalLength(fd.totalLength);
+        memcpy(targetMesg->GetAddressRxBuffer() + i, periphBuffer, 1);
+        cout << "Receive byte: " << (int)i << " total length: " << std::dec << (int)fd.totalLength << endl;
         i++;
     } 
     else {
         memcpy(periphBuffer,testBuffer + i, 1);
-        memcpy(Receiver.GetAddressRxBuffer() + i, periphBuffer, 1);
-        cout << "Receive byte: " << (int)i << " value: " << (int)*(Receiver.GetAddressRxBuffer() + i) << endl;
+        memcpy(targetMesg->GetAddressRxBuffer() + i, periphBuffer, 1);
+        cout << "Receive byte: " << (int)i << " value: 0x" << std::hex << (int)*(targetMesg->GetAddressRxBuffer() + i) << endl;
         i++;
         // Nếu đủ độ dài thì đã kết thúc frame truyền, thực hiện xóa bộ đệm _txBuffer
         if(i == fd.totalLength) {
             cout << "Receive complete" << endl;
             // Xóa bộ đệm test sau khi nhận đầy đủ
             memset(testBuffer, 0, i);
+            Device1.SetSendingStatus(false);
+            Device2.SetSendingStatus(false);
             // Sau khi nhận đầy đủ khung truyền thì giải mã khung truyền, kiểm tra CRC16
-            Receiver.HandleReceiveMessage();
-            Receiver.ResetFrame();
+            targetMesg->HandleReceiveMessage();
             i = 0;
         }
     }
     
 }
 
-
-
-void BasicTest(){
-    uint8_t testValueSend = 10;
-    Sender.SendMessage((void*)&testValueSend, sizeof(uint8_t), PROTOCOL_ID_1);
+void TestSendData(){
+    uint8_t testValueSend = 13;
+    Device1.SendMessage((void*)&testValueSend, sizeof(uint8_t), PROTOCOL_ID_1);
 }
 
 int main(void){
-    BasicTest();
+     Device1.RequestHandshake();
     while(1) {
         SimulatePeripheralSendMessage();
         SimulatePeripheralReceiveMessage();

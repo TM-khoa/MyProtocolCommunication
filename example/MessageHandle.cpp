a49261f71cc09bc831bbe4c028815bccb4b42173
TM-khoa <minhkhoa9a31314@gmail.com>
2024-07-17 19:05:44 +0700

    feat: add func request and response handshake code in messagehandle

--- example/MessageHandle.cpp
+++ example/MessageHandle.cpp
@@ -2,31 +2,36 @@
 #include <iostream>
 
 
-
 void PrintFrameData(FrameData fd){
     
     // ép kiểu int để cout in ra dạng số thay vì ký tự ASCII
-    std::cout << "totalLength:" << (int)fd.totalLength << std::endl;
-    std::cout << "protocolID:" << (int)fd.protocolID << std::endl;
-    std::cout << "requestData:" << (int)fd.requestData << std::endl;
-    std::cout << "payload length:" << (int)fd.payloadLength << std::endl;
-    std::cout << "crc16:0x" << std::hex << fd.crc16 << std::endl;
+    std::cout << "\t" << "totalLength:" << (int)fd.totalLength << std::endl;
+    std::cout << "\t" << "protocolID:" << (int)fd.protocolID << std::endl;
+    std::cout << "\t" << "requestData:" << (int)fd.requestData << std::endl;
+    std::cout << "\t" << "payload length:" << (int)fd.payloadLength << std::endl;
+    std::cout << "\t" << "crc16:0x" << std::hex << fd.crc16 << std::endl;
 }
 
 int MessageHandle::SendMessage(ProtocolListID id, bool requestData){
+    if(_isSending == true) return 1;
     FrameData fd  = Protocol::MakeFrame(id,requestData);
     std::cout << "Virtual send message" <<  std::endl;
     PrintFrameData(fd);
     Protocol::ResetFrame();
+    _isSending = true;
     return 0;
 }
 
 int MessageHandle::SendMessage(void* payload, uint16_t sizeOfPayload, ProtocolListID id){
+    if(_isSending == true) return 1;
     FrameData fd  = Protocol::MakeFrame(payload,sizeOfPayload,id);
     std::cout << "Virtual send message" <<  std::endl;
     PrintFrameData(fd);
+    _isSending = true;
     return 0;
 }
+
+
 
 int MessageHandle::HandleReceiveMessage(){
     std::cout << "Virtual receive message" << std::endl;
--- example/MessageHandle.h
+++ example/MessageHandle.h
@@ -21,11 +21,32 @@ class MessageHandle : public Protocol
     virtual int SendMessage(ProtocolListID id, bool requestData);
     virtual int SendMessage(void* payload, uint16_t sizeOfPayload, ProtocolListID id);
     virtual int HandleReceiveMessage();
+    uint8_t* GetHandshakeCode(){return (uint8_t*)_handshakeCode;}
+    void SetHandshake(bool handshake){_isHandshake = handshake;}
+    bool GetHandshakeStatus(){return _isHandshake;}
+    void SetSendingStatus(bool isSending) {_isSending = isSending;}
+    bool GetSendingStatus(){return _isSending;}
+    int RequestHandshake(){
+        if(_isSending == true) return 1;
+        Protocol::SendRequestHandshakeCode((uint8_t*)_handshakeCode,sizeof(_handshakeCode),PROTOCOL_ID_HANDSHAKE);
+        _isSending = true;
+        return 0;
+    }
 
+    int ResponseHandshake(){
+        if(_isSending == true) return 1;
+        Protocol::SendResponseHandshakeCode((uint8_t*)_handshakeCode,sizeof(_handshakeCode),PROTOCOL_ID_HANDSHAKE);
+        _isSending = true;
+        return 0;
+    }
 
     private:
 	uint8_t _rxBuf[50];
 	uint8_t _txBuf[50];
+    const uint8_t _handshakeCode[8] = {0x12,0x34,0x56,0x78,0x90,0xab,0xcd,0xef};
+    bool _isHandshake = false;
+    bool _isSending = false;
+
 };
 
 #endif
